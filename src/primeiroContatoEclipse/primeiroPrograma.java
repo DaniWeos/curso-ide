eba74e163c17de4998a3991fce8446ec113dec3e
DaniWeos <danielweoswork@gmail.com>
2023-02-17 03:23:28 -0300

    Primeiro Commit

---
+++ src/primeiroContatoEclipse/primeiroPrograma.java
@@ -0,0 +1,26 @@
+package primeiroContatoEclipse;
+
+import primeiroContatoEclipseGato.Gato;
+
+public class primeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+				
+		System.out.println(gato); 
+				
+				
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Boa tarde Daniel! " + (a+b));*/
+	}
+
+}
+
+class Livros{
+	
+	private String nome;
+	private Integer npag;
+}
